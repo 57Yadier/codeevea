2b233459217e3db2dec37adf12a5d08fd4bce238
57Yadier <103086948+57Yadier@users.noreply.github.com>
2022-04-07 09:45:13 -0400

    Update comfig.py

--- comfig.py
+++ comfig.py
@@ -1,10 +1,10 @@
-BOT_TOKEN = ''
+BOT_TOKEN = '5207968991:AAGHHUB5f_Uo6ggXkkTxisalJD5Yv7EpWI4'
 API_ID = ''
 API_HASH = ''
-ADMIN_ID = []
+ADMIN_ID = [1850218220]
 
-MOODLE_URL = ''
-MOODLE_USER = ''
-MOODLE_PASSWORD = ''
-PARTES_MB = 
+MOODLE_URL = 'https://evea.uh.cu/'
+MOODLE_USER = 'darian.borges@estudiantes.fbio.uh.cu'
+MOODLE_PASSWORD = 'darian1995'
+PARTES_MB = 100
 
